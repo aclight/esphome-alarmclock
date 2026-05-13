8327a4da0fc8aab2cdb7b2afac6b2addaa288b2d
Adam Light <aclight@gmail.com>
2026-05-13 03:51:08 +0000

    Fix clock_font_120 linkage: add extern declaration for external visibility

--- components/alarmclock/clock_font_120.cpp
+++ components/alarmclock/clock_font_120.cpp
@@ -5367,6 +5367,7 @@ static lv_font_fmt_txt_dsc_t font_dsc = {
 
 /*Initialize a public general font descriptor*/
 #if LVGL_VERSION_MAJOR >= 8
+extern const lv_font_t clock_font_120;
 const lv_font_t clock_font_120 = {
 #else
 lv_font_t clock_font_120 = {
