091c5d718a97f05569f449625df8ffe87eb8aa96
Adam Light <aclight@gmail.com>
2026-09-10 20:20:24 -0700

    Set LVGL stdio FS drive letter via LV_FS_STDIO_LETTER

--- components/sd_card_proof/__init__.py
+++ components/sd_card_proof/__init__.py
@@ -23,7 +23,8 @@ def _add_lvgl_defines(config):
     # lv_image.h hard-requires the label widget even though our page has none.
     lv_defines.add_define("LV_USE_LABEL", "1")
     lv_defines.add_define("LV_USE_TJPGD", "1")
-    lv_defines.add_define("LV_USE_FS_STDIO", "'S'")
+    lv_defines.add_define("LV_USE_FS_STDIO", "1")
+    lv_defines.add_define("LV_FS_STDIO_LETTER", "'S'")
     lv_defines.add_define("LV_FS_STDIO_PATH", '"/sdcard/"')
     return config
 
